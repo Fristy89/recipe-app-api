bdf69c882005eb66c035132fcc55e31bdf598619
Dennis Pieruschka <dpieruschka@gmail.com>
2019-02-15 21:15:01 +0100

    User.is_staff Fix
    Added Users

--- app/core/models.py
+++ app/core/models.py
@@ -2,12 +2,21 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
 PermissionsMixin
 
-class BaseUserManager(BaseUserManager):
+class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new user"""
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError('Users must have an Email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+    def create_superuser(self, email, password):
+        """Create and saves a new super"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
@@ -21,5 +30,3 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     objects = UserManager()
     USERNAME_FIELD = 'email'
-
-# Create your models here.
