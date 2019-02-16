a01129a2d86c39a546c9351d9c9207e90107e22c
Dennis Pieruschka <dpieruschka@gmail.com>
2019-02-16 23:31:32 +0100

    UserADmin settings

--- app/core/models.py
+++ app/core/models.py
@@ -12,6 +12,8 @@ class UserManager(BaseUserManager):
         user.set_password(password)
         user.save(using=self._db)
 
+        return user
+
     def create_superuser(self, email, password):
         """Create and saves a new super"""
         user = self.create_user(email, password)
